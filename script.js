87c26d3e55e39b634af02076b3ae0a977f897bc3
u9454545 <86706891+u9454545@users.noreply.github.com>
2021-07-01 16:37:14 +1000

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,36 @@
 function compute()
 {
     p = document.getElementById("principal").value;
+    var principal = document.getElementById("principal").value;
+    if(principal<1){
+    //result=document.getElementById("result");
+    //result.innerHTML="Enter a positive number";
+    alert("Enter a positive number");
+    }
+    else{
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var interest = principal * years * rate /100;
+    var year = new Date().getFullYear()+parseInt(years);
+    function updateRate() 
+{
+    var rateval = document.getElementById("rate").value;
+    var d =document.getElementById("rate_val");
+    d.innerText=rateval;
     
+    rateval.onchange=function(){
+    d.innerText=rateval;
+    };
+    
+    
+}  
+    result=document.getElementById("result");
+    result.innerHTML="If you deposit "+principal +", "+"at an interest rate of "+rate+"%."+
+    "You will receive an amount of "+interest+" in the year "+year;
+    }
+
+
+    
+
 }
         
